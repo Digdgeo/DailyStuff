a29a7796e572fce079f2abf29b38300992f81aa1
Digdgeo <digd.geografo@gmail.com>
2018-12-12 17:04:59 +0100

    Adding funcionalites to get_escenas_values. Still to check no data and get all statistic rasters

--- get_escenas_values.py
+++ get_escenas_values.py
@@ -1,8 +1,9 @@
 
-
 import os
+import rasterio
 import numpy as np
 from datetime import date
+
 
 
 def get_escenas_values(path):
@@ -11,9 +12,10 @@ def get_escenas_values(path):
     hidrológico. Con suerte, será la base para el cálculo del hidroperiodo'''
     
     
-    escenas = [i[:8] for i in os.listdir(path) if i.endswith('.shp')]
+    escenas = [i[:8] for i in os.listdir(path) if i.endswith('.tif')]
     years = set([i[:4] for i in escenas])
-    d0 = date(int(years), 9, 1)
+    d0 = date(int(min(years)), 9, 1)
+    #print(years)
     
     ndays = []
     nmeds = []
@@ -33,10 +35,31 @@ def get_escenas_values(path):
     for n, e in enumerate(nmeds):
         pcortes.append(e + ndays[n])
 
+    values.append(pcortes[0])
     for n in range(len(pcortes)-1):
         p = (pcortes[n+1] - pcortes[n])
         values.append(p)
+    values.append(365-pcortes[-1])
 
     escenas_valor = dict(zip(sorted(escenas), values))
     
     return escenas_valor
+
+
+def get_hydroperiod(path, values):
+    
+    escenas = [i for i in os.listdir(path) if i.endswith('.tif')]
+        
+    for i in sorted(escenas):
+        rs = os.path.join(path, i)
+        out = os.path.join(path, os.path.join('outputs', i[:-4] + '_rec.tif'))
+        with rasterio.open(rs) as src:
+            RS = src.read()
+            RS[RS == 1] = get_escenas_values('/home/diego/Ecopotential/shapes/tests/rasters/')[i[:8]]
+            print(i, RS.mean())
+            
+            profile = src.meta
+            profile.update(dtype=rasterio.float32)
+
+            with rasterio.open(out, 'w', **profile) as dst:
+                dst.write(RS.astype(rasterio.float32))
